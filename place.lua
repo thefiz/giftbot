0a425da11361319a640ab74b38605892f1490e3a
thefiz <hialan@fastmail.fm>
2023-07-24 03:24:21 +0000

    Add files via upload

---
+++ place.lua
@@ -0,0 +1,16 @@
+{
+    "contact": "https://discord.gg/YzZfdedyT4",
+    "notifications": "",
+    "templates": [
+      {
+        "name": "Green Lattice",
+        "sources": [
+          "https://github.com/artillect/greenlattice-template/blob/main/lattice_template.png?raw=true"
+        ],
+        "x": 1677,
+        "y": 0
+      }
+    ],
+    "whitelist": [],
+    "blacklist": []
+  }
